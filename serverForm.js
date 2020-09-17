96e0415dd2eb688ca1bf3cdb35f50ad356c4e289
foak77 <emailtofred@gmail.com>
2020-09-17 10:48:44 -0400

    Form Validator

---
+++ serverForm.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(`${__dirname}/`));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(3013, () => {
+  console.log(`Server Form Validator on 3013`);
+});
